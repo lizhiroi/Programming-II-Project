eed752b066fd9f78a45cef468e5a2e7c1955f008
BAO Qingjun <aibaoqj@163.com>
2023-07-12 12:14:19 -0400

    first version

---
+++ package3/Multirotor.java
@@ -0,0 +1,57 @@
+package package3;
+
+import package2.Helicopter;
+
+public class Multirotor extends Helicopter{
+	protected int rotorsNumber;
+
+	public Multirotor(String band, double price, int horsepower, int cylinders, int creationYear, int passengerCapacity, int rotorsNumber){
+		super(band, price, horsepower, cylinders, creationYear, passengerCapacity);
+		this.rotorsNumber = rotorsNumber;
+	}
+
+	public Multirotor(int rotorsNumber){
+		super();
+		this.rotorsNumber = rotorsNumber;
+	}
+	
+	public Multirotor(Multirotor multirotor){
+		this(multirotor.band, multirotor.price, multirotor.horsepower, 
+				multirotor.cylinders, multirotor.creationYear, multirotor.passengerCapacity, 
+				multirotor.rotorsNumber);
+	}
+	
+	public Multirotor() {
+		this(0);
+	}
+	
+	public int getRotorsNumber() {
+		return rotorsNumber;
+	}
+	
+	public void setRotorsNumber(int rotorsNumber) {
+		this.rotorsNumber = rotorsNumber;
+	}
+	
+	@Override
+	public String toString() {
+		 return "Multirotor is kind of " + super.toString()
+				 + ", has "+ rotorsNumber + " rotors.";
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(o == this) {
+			return true;
+		}
+		
+		if(o instanceof Multirotor) {
+			Multirotor q = (Multirotor) o;
+			if(super.equals(q) && q.rotorsNumber == this.rotorsNumber) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+}
---
+++ package4/UAV.java
@@ -0,0 +1,57 @@
+package package4;
+
+public class UAV {
+	protected double weight;
+	protected double price;
+
+	public UAV(double weight, double price){
+		this.weight = weight;
+		this.price = price;
+	}
+
+	public UAV(UAV uav){
+		this(uav.weight, uav.price);
+	}
+	
+	public UAV() {
+		this(0.0, 0.0);
+	}
+	
+	public double getWeight() {
+		return weight;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		 return "UAV(Unmanned aerial vehicle / Drone), it weighs " + weight + " pounds"
+				 + ", the price is " + String.format("%.2f",price)+"$";
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(o == this) {
+			return true;
+		}
+		
+		if(o instanceof UAV) {
+			UAV u = (UAV) o;
+			if(u.weight == this.weight && u.price == this.price) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+}
