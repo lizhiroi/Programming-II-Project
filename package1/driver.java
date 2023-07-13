ba49e67662dccc9adba6ed28376304942e14b7cf
BAO Qingjun <aibaoqj@163.com>
2023-07-13 01:20:35 -0400

    final

--- package1/driver.java
+++ package1/driver.java
@@ -8,40 +8,51 @@ import package5.AgriculturalDrone;
 import package5.MAV;
 
 public class driver {
-	public static void findLeastAndMostExpensiveUAV(Object[] obj) {
-			if(!(obj[0] instanceof UAV)) {
-				System.out.println("This flying object is not a UAV.");
-			} else {
-				if(obj.length==1) {
-					UAV uav = (UAV) obj[0];
-					System.out.println("The UAV array has only 1 UAV, the Least and Most Expensive UAV is $" 
-									+ String.format("%.2f",uav.getPrice())+"$");
-				} else {
-					double leastPrice=-1;
-					double mostPrice=-1;
-					double price;
-					int leastPriceNum=0;
-					int mostPriceNum=0;
-					int count = 0;
-					
-					for(Object o : obj) {
-						UAV uav = (UAV) o;
-						count++;
-						price = uav.getPrice();
-						if(leastPrice==-1 || leastPrice>price) {
-							leastPrice = price;
-							leastPriceNum = count;
-						}
-						if(mostPrice==-1 || mostPrice<price) {
-							mostPrice = price;
-							mostPriceNum = count;
-						}
-					}
-					System.out.println("The Least Expensive UAV is No."+leastPriceNum+" costs $"	+ String.format("%.2f", leastPrice));
-					System.out.println("The Most Expensive UAV is No."+mostPriceNum+" costs $" + String.format("%.2f", mostPrice));
+	
+	public static void findLeastAndMostExpensiveUAV(Object[] object) {
+		double leastPrice=-1;
+		double mostPrice=-1;
+		double price=0;
+		int leastPriceNum=0;
+		int mostPriceNum=0;
+		
+		int count = 0;
+		int uavNum = 0;
+
+		for(Object o : object) {
+			if(o instanceof UAV || o instanceof AgriculturalDrone || o instanceof MAV) {
+				uavNum++;
+				price = ((UAV) o).getPrice();
+				if(uavNum==1) {
+					leastPrice = price;
+					mostPrice = price;
+					leastPriceNum = count;
+					mostPriceNum = count;
+				}
+				
+				if(price<leastPrice) {
+					leastPrice = price;
+					leastPriceNum = count;
+				}
+				if(price>mostPrice) {
+					mostPrice = price;
+					mostPriceNum = count;
 				}
 			}
-			
+			count++;
+		}
+		
+		if(uavNum==0) {
+			System.out.println("There is NO any UAV flying object in this array.");
+		} else if(uavNum==1) {
+			System.out.println("The only UAV flying object is a(n) " + object[0].getClass() + ", its price is $"
+						+ String.format("%.2f", price));
+		} else {
+			System.out.println("The Least Expensive UAV is a(n) " + objName(object[leastPriceNum]) + ", the price is $"
+					+ String.format("%.2f", leastPrice));
+			System.out.println("The  Most Expensive UAV is a(n) " + objName(object[mostPriceNum]) + ", the price is $"
+					+ String.format("%.2f", mostPrice));
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -89,22 +100,36 @@ public class driver {
 		Multirotor multirotor4=new Multirotor(multirotor3);
 		System.out.println(multirotor3);
 		
+		System.out.println("\nInitializing UAV flying objects...\n");
+
 //		public UAV(double weight, double price)
 		UAV uav1=new UAV(33, 250);
 		System.out.println(uav1);
 		
+		UAV uav2=new UAV(44, 360);
+		System.out.println(uav2);
+		
+		UAV uav3=new UAV(55, 480);
+		System.out.println(uav3);
+		
 //		public AgriculturalDrone(double weight, double price, String brand, int carryCapacity)
 		AgriculturalDrone agriculturalDrone1=new AgriculturalDrone(55, 3600, "MeasurDrone", 60);
 		System.out.println(agriculturalDrone1);
+
+		AgriculturalDrone agriculturalDrone2=new AgriculturalDrone(55, 3700, "MeasurDrone", 60);
+		System.out.println(agriculturalDrone2);
+		
+		AgriculturalDrone agriculturalDrone3=new AgriculturalDrone(agriculturalDrone1);
+		System.out.println(agriculturalDrone3);
 		
 //		public MAV(double weight, double price, String model, double size)
-		MAV mav1=new MAV(55, 3600, "MavTech Q4T", 860);
+		MAV mav1=new MAV(55, 150, "MavTech Q4T", 860);
 		System.out.println(mav1);
 		
-		MAV mav2=new MAV(uav1, "Honeywell", 550);
+		MAV mav2=new MAV(33, 250, "Honeywell", 550);
 		System.out.println(mav2);
 
-		MAV mav3=new MAV(33, 250, "Honeywell", 550);
+		MAV mav3=new MAV(uav2, "Honeywell", 550);
 		System.out.println(mav3);
 		
 //		Test the equality of some of the created objects using the equals() method. You should test at least
@@ -151,53 +176,72 @@ public class driver {
 		
 		
 //		Create 2 arrays, each of 15 to 20 of these flying objects and fill these arrays with various objects
-//		from these classes. The first array must include at least one object from each of the classes; while
-//		the second array should not have any UAV objects.
-		Object[] flyingObj = new Object[15];
-		flyingObj[0] = airplane1;
-		flyingObj[1] = helicopter1;
-		flyingObj[2] = quadcopter1;
-		flyingObj[3] = multirotor1;
-		flyingObj[4] = multirotor2;
-		flyingObj[5] = multirotor3;
-		flyingObj[6] = multirotor4;
-		flyingObj[7] = uav1;
-		flyingObj[8] = agriculturalDrone1;
-		flyingObj[9] = mav1;
-		flyingObj[10] = mav2;
-		flyingObj[11] = mav3;
-//		flyingObj[12] = multirotor4;
-//		flyingObj[13] = multirotor4;
-//		flyingObj[14] = multirotor4;
-//		flyingObj[15] = multirotor4;
-//		
+//		from these classes. The first array must include at least one object from each of the classes
 		
-		
-//		 Finally call the findLeastAndMostExpensiveUAV() once with the first array as a parameter and
-//		 once with the second array. This should display what is needed!
-//		String [] str;
-//		String flyingType;
-		
-		for(Object obj : flyingObj) {
-			String str = obj.getClass().toString();
-//			String str = obj.getClass().getSuperclass().getName().toString();
-			String [] s=str.split("\\.", 0);
-			String flyingType = s[s.length-1];
-			
-			System.out.println(str +"      "+ flyingType);
-			
-			if(flyingType=="Airplane") {
-				Airplane ap = (Airplane) obj;
-				System.out.println(ap.getPrice());
-			}
-			
-			if(flyingType=="UAV") {
-				UAV ap = (UAV) obj;
-				System.out.println(ap.getPrice());
-			}
-			
-		}
+		System.out.println("\nInitializing two arrays of flying objects...\n");
+		System.out.println("The first array of flying objects...");
 
+		Object[] flyingObj1 = new Object[16];
+		flyingObj1[0] = airplane1;
+		flyingObj1[1] = helicopter1;
+		flyingObj1[2] = quadcopter1;
+		flyingObj1[3] = multirotor1;
+		flyingObj1[4] = multirotor2;
+		flyingObj1[5] = multirotor3;
+		flyingObj1[6] = multirotor4;
+		flyingObj1[7] = uav1;
+		flyingObj1[8] = uav2;
+		flyingObj1[9] = uav3;
+		flyingObj1[10] = agriculturalDrone1;
+		flyingObj1[11] = agriculturalDrone2;
+		flyingObj1[12] = agriculturalDrone3;
+		flyingObj1[13] = mav1;
+		flyingObj1[14] = mav2;
+		flyingObj1[15] = mav3;
+		
+//		while the second array should not have any UAV objects.
+		
+		System.out.println("The second array of flying objects...");
+
+		Object[] flyingObj2 = new Object[20];
+		flyingObj2[0] = airplane1;
+		flyingObj2[1] = helicopter1;
+		flyingObj2[2] = quadcopter1;
+		flyingObj2[3] = multirotor1;
+		flyingObj2[4] = multirotor2;
+		flyingObj2[5] = multirotor3;
+		flyingObj2[6] = multirotor4;
+		flyingObj2[7] = airplane1;
+		flyingObj2[8] = helicopter1;
+		flyingObj2[9] = quadcopter1;
+		flyingObj2[10] = multirotor1;
+		flyingObj2[11] = multirotor2;
+		flyingObj2[12] = multirotor3;
+		flyingObj2[13] = multirotor4;		
+		flyingObj2[14] = airplane1;
+		flyingObj2[15] = helicopter1;
+		flyingObj2[16] = quadcopter1;
+		flyingObj2[17] = multirotor1;
+		flyingObj2[18] = multirotor2;
+		flyingObj2[19] = multirotor3;
+		
+//		Finally call the findLeastAndMostExpensiveUAV() once with the first array as a parameter and
+//		once with the second array. This should display what is needed!
+		
+		System.out.println("\nChecking out the first array...\n");
+
+		findLeastAndMostExpensiveUAV(flyingObj1);
+
+		System.out.println("\nChecking out the second array...\n");
+		
+		findLeastAndMostExpensiveUAV(flyingObj2);
+		
 	}
-
+	
+	public static String objName(Object object) {
+		String str = object.getClass().toString();
+		String [] s=str.split("\\.", 0);
+		
+		return s[s.length-1];
+	}
 }
